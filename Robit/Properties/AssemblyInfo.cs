59cfe1b2421b667e0055b42a34f6f804afa27f2b
Wesley <lwpmoon@me.com>
2016-04-01 21:17:56 -0600

    Added system.Diagnostics to Assemblyinfo for debug statements.

--- Robit/Properties/AssemblyInfo.cs
+++ Robit/Properties/AssemblyInfo.cs
@@ -23,3 +23,24 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+namespace System.Diagnostics
+{
+    //  DebuggerBrowsableState states are defined as follows:
+    //      Never       never show this element
+    //      Expanded    expansion of the class is done, so that all visible internal members are shown
+    //      Collapsed   expansion of the class is not performed. Internal visible members are hidden
+    //      RootHidden  The target element itself should not be shown, but should instead be
+    //                  automatically expanded to have its members displayed.
+    //  Default value is collapsed
+
+    //  Please also change the code which validates DebuggerBrowsableState variable (in this file)
+    //  if you change this enum.
+    public enum DebuggerBrowsableState
+    {
+        Never = 0,
+        Collapsed = 2,
+        RootHidden = 3
+    }
+}
